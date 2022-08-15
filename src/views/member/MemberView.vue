f6394061ef31922ae502232b7f8793f026d7c30c
30382718ZiLongWu <1562594113@qq.com>
2022-08-15 21:25:55 +0800

    查看

--- src/views/member/MemberView.vue
+++ src/views/member/MemberView.vue
@@ -6,9 +6,9 @@
       <div class="user-info-box">
         <div class="user-base-info-box">
           <div class="user-avatar">
-            <img :src="uesrData.avatar" alt="" />
+            <img :src="userData.avatar" alt="" />
           </div>
-          <div class="user-nickname">{{ uesrData.nick_name }}</div>
+          <div class="user-nickname">{{ userData.nick_name }}</div>
           <div class="buttons">
             <el-button
               type="primary"
@@ -22,7 +22,7 @@
               style="margin-right: 20px"
               :underline="false"
               target="_blank"
-              @click.stop="goEdit(uesrData.id)"
+              @click.stop="goEdit(userData.id)"
               >修改资料</el-link
             >
             <el-link
@@ -30,7 +30,7 @@
               style="margin-right: 20px"
               :underline="false"
               target="_blank"
-              @click.stop="goCredit1(uesrData.id)"
+              @click.stop="goCredit1(userData.id)"
               >修改积分</el-link
             >
             <el-link
@@ -38,7 +38,7 @@
               style="margin-right: 20px"
               :underline="false"
               target="_blank"
-              @click.stop="goTags(uesrData.id)"
+              @click.stop="goTags(userData.id)"
               >修改标签</el-link
             >
             <el-link
@@ -46,7 +46,7 @@
               style="margin-right: 20px"
               :underline="false"
               target="_blank"
-              @click.stop="goRemark(uesrData.id)"
+              @click.stop="goRemark(userData.id)"
               >修改备注</el-link
             >
           </div>
@@ -54,22 +54,22 @@
       </div>
 
       <div class="panel-info-box">
-        <div class="panel-info-item">ID: {{uesrData.id}}</div>
-        <div class="panel-info-item">手机号: {{uesrData.mobile}}</div>
-        <div class="panel-info-item">积分: {{uesrData.credit1}}</div>
-        <div class="panel-info-item">VIP: {{uesrData.role.name}}</div>
-        <div class="panel-info-item">VIP过期时间: {{uesrData.role_expired_at}}</div>
-        <div class="panel-info-item">一级邀请人: {{uesrData.invitor==null?'':uesrData.invitor}}</div>
-        <div class="panel-info-item">学员邀请码: {{uesrData.is_used_promo_code==0?'未使用':uesrData.is_used_promo_code}}</div>
-        <div class="panel-info-item">推广余额: {{uesrData.invite_balance}}</div>
-        <div class="panel-info-item">锁定登录: {{uesrData.is_lock==0?'是':'否'}}</div>
+        <div class="panel-info-item">ID: {{userData.id}}</div>
+        <div class="panel-info-item">手机号: {{userData.mobile}}</div>
+        <div class="panel-info-item">积分: {{userData.credit1}}</div>
+        <div class="panel-info-item">VIP: {{userData.role.name}}</div>
+        <div class="panel-info-item">VIP过期时间: {{userData.role_expired_at}}</div>
+        <div class="panel-info-item">一级邀请人: {{userData.invitor==null?'':userData.invitor}}</div>
+        <div class="panel-info-item">学员邀请码: {{userData.is_used_promo_code==0?'未使用':userData.is_used_promo_code}}</div>
+        <div class="panel-info-item">推广余额: {{userData.invite_balance}}</div>
+        <div class="panel-info-item">锁定登录: {{userData.is_lock==0?'是':'否'}}</div>
         <div class="panel-info-item">IP地址: </div>
         <div class="panel-info-item">注册区域:</div>
         <div class="panel-info-item">标签:
-            <el-tag style="marginLeft:5px" v-for="item in uesrData.tags" :key="item">{{item.name}}</el-tag>
+            <el-tag style="marginLeft:5px" v-for="item in userData.tags" :key="item">{{item.name}}</el-tag>
         </div>
         <div class="panel-info-item" >备注:
-            <div v-if="uesrData.remark!=null" v-html="uesrData.remark.remark"></div>
+            <div v-if="userData.remark!=null" v-html="userData.remark.remark"></div>
         </div>
       </div>
     </div>
@@ -89,7 +89,7 @@ export default {
     //这里存放数据
     return {
       id: "",
-      uesrData: "",
+      userData: "",
       userCourses: "",
     };
   },
@@ -131,7 +131,7 @@ export default {
   async created() {
     this.id = this.$route.params.id;
     let userRes = await this.$request("/member/" + this.id + "/detail");
-    this.uesrData = userRes.data.data;
+    this.userData = userRes.data.data;
 
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
